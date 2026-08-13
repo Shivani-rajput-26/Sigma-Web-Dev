7616f43be3ac0adab92600f8e4dd3d18a75d77de
Shivani Rajput <shivani26.exe@gmail.com>
2026-08-13 14:41:58 +0530

    Intro of js

---
+++ Video54/script.js
@@ -0,0 +1,20 @@
+alert("Hello World");
+
+console.log("Code is running....")
+console.log("Code is also running....")
+console.log("Code is looking like a woww....")
+
+var a = prompt("ENter your number")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+}
+
+console.log("Your number is " + a)
+
+document.title = "Hey I am Good"
+// document.body.style.background = "purple"
