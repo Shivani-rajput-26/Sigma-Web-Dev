33c8a4b9d78290503b0c6211cc6cad1b7ec0f92d
Shivani Rajput <shivani26.exe@gmail.com>
2026-08-14 18:35:19 +0530

    JavaScript Variables

---
+++ Video55/script.js
@@ -0,0 +1,50 @@
+console.log("Hey this is tutorial 55")
+
+let a = 5; // var ki jgh let ka use krenge kuki let likhte hi vo ek global variable bn gya
+
+// a = a+1;
+let b = 6;
+let c = "Shivani";
+let _a = "Shuham";
+// var 55a = "Rohan"; ==> Not allowed
+
+
+
+// console.log(a+b+8)
+// console.log(typeof a, typeof b, typeof c)
+{
+    let a = 66; //Let means it is an block scoped
+    console.log(a)
+}
+console.log(a)
+
+
+
+// const a1 = 6;
+// a1 = a1 + 1; ==>> It means hum constant ki value ko chnage nhi kr skte
+
+
+
+let x = "Shivani";
+let y = 22
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+// OBJECT ----->> Object is a key value pairs in javascript
+
+let o ={
+    "name": "Harry",
+    "jobe role": 5600,
+    "is_handsome": true,
+}
+
+console.log(o);
+o.salary = "100 Crores";
+console.log(o);
+o.salary = "500 Crores";
+console.log(o)
