d5a886b5d59adbb1cc10684ea3357214eab041f7
Shivani Rajput <shivani26.exe@gmail.com>
2026-08-18 18:01:55 +0530

    Loops in JS

---
+++ Video57/script.js
@@ -0,0 +1,40 @@
+console.log("I am a student to study about Loops")
+
+let a = 1;
+
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+    
+// }
+
+// let obj = {
+//     name: "Shivani",
+//     role: "Programmer",
+//     company: "Fintech"
+// }
+
+// for (const key in obj) {
+//     console.log(key, element)    
+// }
+
+// for (const c of "Shivani"){
+//     console.log(c)
+// }
+
+
+// let i = 0;
+// while (i < 6) {
+//     console.log(i)
+//     i++;
+// }
+
+
+let i = 0;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
