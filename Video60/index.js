768f03430dab46e81e0498d7663c5ff73299439b
Shivani Rajput <shivani26.exe@gmail.com>
2026-08-21 20:30:17 +0530

    Strings in JS

---
+++ Video60/index.js
@@ -0,0 +1,26 @@
+console.log("This is strings tutorial")
+let a = "Shivani";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+
+console.log(a.length)
+
+let name = "Shivani"
+let friend = "Deepak"
+console.log("Her name is " + name +" and her friend name is " + friend)
+console.log(`Her name is ${name} and her friend name is ${friend}`)
+
+let b = "TanishaSh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,5))
+console.log(b.slice(2))
+
+console.log(b.replace("Sh", "26"))
+console.log(b.concat(a, "Ketrina", "Rahul") )
