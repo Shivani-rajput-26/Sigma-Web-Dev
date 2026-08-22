2ba24aaab70c34efcdb3d3d883f1338d3a971d14
Shivani Rajput <shivani26.exe@gmail.com>
2026-08-22 18:07:37 +0530

    Faulty Calculator

---
+++ Faulty Calculator 59/index.js
@@ -0,0 +1,38 @@
+/* Create a faulty calculator using JavaScript
+
+This faulty calculator does following:
+1. It takes two numbers as input from the user
+2. It performs wrong operation as follows:
++ ----> -
+* ----> +
+- -----> /
+/ ----> **
+
+
+It performs wrong operation 10% of the times
+*/ 
+
+
+let random = Math.random()
+console.log(random)
+let a = prompt("Enter first number")
+let b = prompt("Enter second number")
+let c = prompt("Enter operation")
+
+let obj = {
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**",
+}
+
+if(random > 0.1){
+    //Perform correct calculation
+    console.log(`The result is ${a} ${c} ${b}`)
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+}
+
+else{
+    //perform wrong calculation
+    c = obj[c]
+}
